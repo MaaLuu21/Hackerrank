61d5f699315f79129b9aef7e22d746b76b1a518c
Maria Luiza <marialuizapaes22@gmail.com>
2025-10-27 22:57:39 -0300

    desafios do hackerrank com testes diferentes

---
+++ StairCase/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://akusing System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        /*char[] escada = new char[n]; 
+
+        for(int i = 0; i < n; i++)
+        {
+            escada[i] = ' ';
+        }
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            escada[i] = '#';
+            Console.WriteLine(new string (escada));
+        }
+        */
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                Console.Write(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                Console.Write("#");
+            }
+            
+        Console.WriteLine("");
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Insira o valor: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Result.staircase(n);
+    }
+}
---
+++ miniMaxSum/Program.cs
@@ -0,0 +1,66 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        int size = arr.Count;
+
+        long sum = arr.Select(x => (long)x).Sum();
+
+        /*
+        long sum = 0;
+        for (int i = 0; i < size; i++)
+        {
+            soma += arr[i];
+        }
+        */
+        long min = arr.Min();
+
+        /*
+        long min = arr[0];
+        for (int i = 1; i < size; i++)
+        {
+            if (arr[i] < arr[0])
+            {
+                min = arr[i];
+            }
+        }
+        */
+
+        long max = arr.Max();
+
+        /*
+        long max = arr[0];
+        for (int i = 1; i < size; i++)
+        {
+            if (arr[i] > max)
+            {
+                max = arr[i];
+            }
+        }
+        */
+
+        long maxSum = sum - min;
+        long minSum = sum - max;
+
+        Console.Write(minSum + " " + maxSum);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.miniMaxSum(arr);
+    }
+}
