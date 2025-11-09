bfafcd39915e53d657dd9b3eb84732d5095824d9
Maaluu21 <marialuizapaes22@gmail.com>
2025-11-09 18:14:30 -0300

    candles

---
+++ birthdayCakeCandles/Program.cs
@@ -0,0 +1,46 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int tallest = 0;
+        int count = 0;
+
+
+        foreach(int candle in candles)
+        {
+            
+            tallest = Math.Max(tallest, candle);
+            if (candle == tallest)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+
+        Console.WriteLine(result);
+    }
+}
